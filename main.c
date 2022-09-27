efd86edf2647244438f43d7384fec441139fafaa
sj332 <lsj02@sookmyung.ac.kr>
2022-09-27 17:22:41 +0900

    Update main.c

--- main.c
+++ main.c
@@ -6,12 +6,9 @@
 int main(int argc, char *argv[]) {
 	int x;
 	
-	printf("input year:");
+	printf("input the second:");
 	scanf("%i",&x);
 	
-	if((x%4==0 && x%100!=0 )|| x%400==0)
-		printf("%i is leap year",x);
-	else
-		printf("%i is not leap year!\n",x);
+	printf("The time for %i is %i : %i : %i",x,x/3600,(x%3600)/60,x%60);
 	return 0;
 }
